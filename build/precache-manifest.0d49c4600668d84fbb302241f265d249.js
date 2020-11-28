8dded79ecd0931bc42a1fad51ee0d3de7eb4eb85
baishaojie <674791374@qq.com>
2020-11-28 13:41:32 +0800

    first commit

---
+++ build/precache-manifest.0d49c4600668d84fbb302241f265d249.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc44f7a5c06e5b68516ca0d3549ea889",
+    "url": "/sushiFrontend/index.html"
+  },
+  {
+    "revision": "e5a85c2e82a58e74cfea",
+    "url": "/sushiFrontend/static/css/main.9bc6d313.chunk.css"
+  },
+  {
+    "revision": "4c2c05be341b593fb6a1",
+    "url": "/sushiFrontend/static/js/0.c79b7d05.chunk.js"
+  },
+  {
+    "revision": "ac2de320ea6a0abda2b8",
+    "url": "/sushiFrontend/static/js/10.479ca3e3.chunk.js"
+  },
+  {
+    "revision": "ebc6f349370c7c641615",
+    "url": "/sushiFrontend/static/js/11.e7014088.chunk.js"
+  },
+  {
+    "revision": "c6e9f7e1b9cf4cb5ad64",
+    "url": "/sushiFrontend/static/js/3.231e94b7.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/sushiFrontend/static/js/3.231e94b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9f2f8cd2b4483a7705",
+    "url": "/sushiFrontend/static/js/4.f27d9b0f.chunk.js"
+  },
+  {
+    "revision": "1152937c963adde9f6ab",
+    "url": "/sushiFrontend/static/js/5.af7b4bc2.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/sushiFrontend/static/js/5.af7b4bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca5204835ca39e99656",
+    "url": "/sushiFrontend/static/js/6.d4e69686.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/sushiFrontend/static/js/6.d4e69686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3593e6225fdd859e1287",
+    "url": "/sushiFrontend/static/js/7.1d1dfb46.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/sushiFrontend/static/js/7.1d1dfb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186a64e63c456d440eed",
+    "url": "/sushiFrontend/static/js/8.ac0458f9.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/sushiFrontend/static/js/8.ac0458f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3266dff49873cc4b19",
+    "url": "/sushiFrontend/static/js/9.6e5bc6a4.chunk.js"
+  },
+  {
+    "revision": "e5a85c2e82a58e74cfea",
+    "url": "/sushiFrontend/static/js/main.d7a512b2.chunk.js"
+  },
+  {
+    "revision": "a0ca8556936577acf01d",
+    "url": "/sushiFrontend/static/js/runtime-main.1480125a.js"
+  },
+  {
+    "revision": "ec78e8e86a08b2278fa9baf09388d697",
+    "url": "/sushiFrontend/static/media/chef.ec78e8e8.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/sushiFrontend/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/sushiFrontend/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
